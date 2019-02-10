5310246f96cd02a1d25b35b61d35cadcc35e980e
nguyentrongvu90 <nguyentrongvu90@gmail.com>
2019-02-10 17:01:55 +0900

    State Pattern

---
+++ src/state/GumballMachine.java
@@ -0,0 +1,93 @@
+package state;
+
+public class GumballMachine {
+
+    private State soldOutState;
+    private State noQuarterState;
+    private State hasQuarterState;
+    private State soldState;
+    private State winnerState;
+
+    private State state = soldOutState;
+    private int count = 0;
+
+    public GumballMachine(int numberGumballs) {
+        soldOutState = new SoldOutState(this);
+        noQuarterState = new NoQuarterState(this);
+        hasQuarterState = new HasQuarterState(this);
+        soldState = new SoldState(this);
+        winnerState = new WinnerState(this);
+        this.count = numberGumballs;
+        if (numberGumballs > 0) {
+            state = noQuarterState;
+        }
+    }
+
+    public void insertQuarter() {
+        state.insertQuarter();
+    }
+
+    public void ejectQuarter() {
+        state.ejectQuarter();
+    }
+
+    public void turnCrank() {
+        state.turnCrank();
+        state.dispense();
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public void releaseBall() {
+        System.out.println("A gumball comes rolling out the slot...");
+        if (count != 0) {
+            count = count - 1;
+        }
+    }
+
+    void refill(int count) {
+        this.count += count;
+        System.out.println("The gumball machine was just refilled; it's new count is: " + this.count);
+        state.refill();
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public State getSoldState() {
+        return soldState;
+    }
+
+    public State getWinnerState() {
+        return winnerState;
+    }
+
+    public State getSoldOutState() {
+        return soldOutState;
+    }
+
+    public State getNoQuarterState() {
+        return noQuarterState;
+    }
+
+    public State getHasQuarterState() {
+        return hasQuarterState;
+    }
+
+    public String toString() {
+        StringBuffer result = new StringBuffer();
+        result.append("\nMighty Gumball, Inc.");
+        result.append("\nJava-enabled Standing Gumball Model #2004");
+        result.append("\nInventory: " + count + " gumball");
+        if (count != 1) {
+            result.append("s");
+        }
+        result.append("\n");
+        result.append("Machine is " + state + "\n");
+        return result.toString();
+    }
+
+}
---
+++ src/state/State.java
@@ -0,0 +1,15 @@
+package state;
+
+public interface State {
+
+    public void insertQuarter();
+
+    public void ejectQuarter();
+
+    public void turnCrank();
+
+    public void dispense();
+
+    public void refill();
+
+}
